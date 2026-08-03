ac854959fd861e4e2e0772323f867d920227ed6c
Anmisha Dash <dashanmisha@gmail.com>
2026-08-03 17:42:22 +0530

    random numbers of dice using ejs

---
+++ index.js
@@ -0,0 +1,19 @@
+const express = require("express");
+const app = express();
+const path = require("path");
+let port = 8080;
+
+app.set("view engine","ejs");
+app.set("views",path.join(__dirname,"/views"));
+
+app.listen(port,()=>{
+    console.log(`listening at port ${port}`);
+});
+
+app.get("/",(req,res)=>{
+   res.render("home.ejs");
+});
+app.get("/rolldice",(req,res)=>{
+    let diceVal = Math.floor(Math.random()*6)+1
+    res.render("rolldice.ejs",{diceVal});
+});
